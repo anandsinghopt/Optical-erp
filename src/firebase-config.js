1f74ae7d7a627bf34eb286098efdc24ecea0d5f0
Anand Singh <anandopt@gmail.com>
2025-06-29 12:51:03 +0300

    Final commit before publishing

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBTvr8DSab1vRXOqLT_JDurZrZiSUDTu7E",
-  authDomain: "optical-erp-4f195.firebaseapp.com",
-  projectId: "optical-erp-4f195",
-  storageBucket: "optical-erp-4f195.appspot.com",
-  messagingSenderId: "147729593640",
-  appId: "1:147729593640:web:b78810f1017535412d8c1d",
-  measurementId: "G-644776QSFD"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
